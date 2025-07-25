9532f924b6d6a14e89307f709977f3394beb256b
restbackDev <jason.rbdev@gmail.com>
2025-07-25 01:58:19 -0700

    feat: edit port-link & typo

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
-    res.json({message: "Hello, Worl!"});
+    res.json({message: "Hello, World!"});
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    // console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on http://127.0.0.1:${port}`);
 });
